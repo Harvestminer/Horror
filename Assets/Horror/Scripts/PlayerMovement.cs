0401f1346b6f60fa09007e177baec6bd6d1aeecd
Harvestminer <harvestminer@gmail.com>
2024-06-04 20:40:37 -0500

    Adds fps movement

--- Assets/Horror/Scripts/PlayerMovement.cs
+++ Assets/Horror/Scripts/PlayerMovement.cs
@@ -8,11 +8,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 #region Fields
-    public float MoveSpeed = 5f;
     public float LookSensitivity = 100f;
-    public Transform player;
+    public float MoveSpeed = 12f;
+    public float JumpHeight = 3f;
+    
+    public Transform Player;
+    public CharacterController Controller;
+
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+
+    private bool isGrounded;
 
     private float yRotation = 0f;
+    private float gravity = -9.81f;
+    private Vector3 velocity;
 #endregion // Fields
 
     void Start()
@@ -21,9 +32,33 @@ public class PlayerMovement : MonoBehaviour
 	}
 
 	void Update()
-	{
+    {
         MouseRotation();
-	}
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        // Move player
+        Vector3 move = Player.transform.right * x + Player.transform.forward * z;
+        Controller.Move(MoveSpeed * Time.deltaTime * move);
+
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        Controller.Move(velocity * Time.deltaTime);
+    }
 
     private void MouseRotation()
     {
@@ -33,7 +68,7 @@ public class PlayerMovement : MonoBehaviour
         yRotation -= mouseY;
         yRotation = Mathf.Clamp(yRotation, -90, 90);
 
-        transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
-        player.Rotate(Vector3.up * mouseX);
+        this.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        Player.Rotate(Vector3.up * mouseX);
     }
 }
